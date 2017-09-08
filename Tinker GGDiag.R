dd2a596a2dbfe179506b67eec210174e40ab210d
jcastagna <31179004+jcastagna@users.noreply.github.com>
2017-09-08 14:21:43 +1000

    Seperate out D1

--- Tinker GGDiag.R
+++ Tinker GGDiag.R
@@ -26,9 +26,10 @@ My.Mod=lm.1
 
 
 plot(My.Mod,which=1)
-D1  = augment(My.Mod) %>% ggplot(aes(x=.fitted,y=.std.resid)) +
-  geom_point()+
-geom_smooth(se=FALSE,colour="red",size=.25)+
+D1  = augment(My.Mod) %>% 
+  ggplot(aes(x=.fitted,y=.std.resid)) +
+  geom_point() +
+  geom_smooth(se=FALSE,colour="red",size=.25) +
   geom_hline(yintercept=0,linetype=3)+
   labs(title="Residuls vs Fitted",subtitle=My.Mod$call)
 D1
